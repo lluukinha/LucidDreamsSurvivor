42c819f8a182edc21835926080579988591a7c65
lucas.souza <lucas.souza@bluecrux.com>
2024-02-12 21:10:43 +0000

    ui iimprovements

--- scenes/managers/enemy_manager.gd
+++ scenes/managers/enemy_manager.gd
@@ -12,6 +12,7 @@ const SPAWN_RADIUS = 375
 var base_spawn_time = 0
 var enemies_table = WeightedTable.new()
 var can_spawn = true
+var number_to_spawn = 1
 
 
 func _ready():
@@ -51,11 +52,12 @@ func on_timer_timeout():
 	
 	timer.start()
 	
-	var enemy_scene = enemies_table.pick_item() as PackedScene
-	var enemy = enemy_scene.instantiate() as Node2D
-	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
-	entities_layer.add_child(enemy)
-	enemy.global_position = get_spawn_position()
+	for i in number_to_spawn:
+		var enemy_scene = enemies_table.pick_item() as PackedScene
+		var enemy = enemy_scene.instantiate() as Node2D
+		var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+		entities_layer.add_child(enemy)
+		enemy.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
@@ -67,4 +69,6 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 		enemies_table.add_item(wizard_enemy_scene, 15)
 	elif arena_difficulty == 18:
 		enemies_table.add_item(bat_enemy_scene, 8)
-		pass
+	
+	if (arena_difficulty % 6) == 0:
+		number_to_spawn +=1
